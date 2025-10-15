18730744ae66a7945c993c2905737f983947f1c8
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2025-10-16 02:27:09 +0530

    app: contract abis

--- app/src/abi/index.ts
+++ app/src/abi/index.ts
@@ -1,16 +1,10 @@
-// ABI exports sourced directly from sibling contracts artifacts
-// Vite server.fs.allow permits reading outside project root
+// ABI exports shipped with the frontend to avoid requiring a contracts compile in CI.
+import CornerstoneProjectArtifact from './CornerstoneProject.json';
+import ProjectRegistryArtifact from './ProjectRegistry.json';
+import IERC20Artifact from './IERC20.json';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - JSON imports with .abi property
-import CornerstoneProjectJson from '../../../contracts/artifacts/src/core/CornerstoneProject.sol/CornerstoneProject.json';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import ProjectRegistryJson from '../../../contracts/artifacts/src/core/ProjectRegistry.sol/ProjectRegistry.json';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import IERC20Json from '../../../contracts/artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json';
+type ArtifactWithAbi = { abi: unknown };
 
-export const CornerstoneProjectABI = (CornerstoneProjectJson as any).abi as any;
-export const ProjectRegistryABI = (ProjectRegistryJson as any).abi as any;
-export const ERC20ABI = (IERC20Json as any).abi as any;
+export const CornerstoneProjectABI = (CornerstoneProjectArtifact as ArtifactWithAbi).abi;
+export const ProjectRegistryABI = (ProjectRegistryArtifact as ArtifactWithAbi).abi;
+export const ERC20ABI = (IERC20Artifact as ArtifactWithAbi).abi;
